2ebfb5f9429deeccc555482b75059ab8d415f0ea
iBog <iBog@users.noreply.github.com>
2023-03-10 15:16:38 +0300

    removed temporary win and linux

--- packages/quick_blue/lib/quick_blue.dart
+++ packages/quick_blue/lib/quick_blue.dart
@@ -4,7 +4,6 @@ import 'dart:typed_data';
 import 'src/quick_blue_platform_interface.dart';
 
 export 'src/models.dart';
-export 'src/quick_blue_linux.dart';
 export 'src/quick_blue_platform_interface.dart';
 
 class QuickBlue {
@@ -16,13 +15,13 @@ class QuickBlue {
 
   static void setInstance(QuickBluePlatform instance) => _platform = instance;
 
-  static void setLogger(QuickLogger logger) =>
-      _platform.setLogger(logger);
+  static void setLogger(QuickLogger logger) => _platform.setLogger(logger);
 
   static Future<bool> isBluetoothAvailable() =>
       _platform.isBluetoothAvailable();
 
-  static Stream<AvailabilityState> get availabilityChangeStream => _platform.availabilityChangeStream.map(AvailabilityState.parse);
+  static Stream<AvailabilityState> get availabilityChangeStream =>
+      _platform.availabilityChangeStream.map(AvailabilityState.parse);
 
   static Future<void> startScan() => _platform.startScan();
 
@@ -30,12 +29,13 @@ class QuickBlue {
 
   static Stream<BlueScanResult> get scanResultStream {
     return _platform.scanResultStream
-      .map((item) => BlueScanResult.fromMap(item));
+        .map((item) => BlueScanResult.fromMap(item));
   }
 
   static Future<void> connect(String deviceId) => _platform.connect(deviceId);
 
-  static Future<void> disconnect(String deviceId) => _platform.disconnect(deviceId);
+  static Future<void> disconnect(String deviceId) =>
+      _platform.disconnect(deviceId);
 
   static void setConnectionHandler(OnConnectionChanged? onConnectionChanged) {
     _platform.onConnectionChanged = onConnectionChanged;
@@ -45,27 +45,38 @@ class QuickBlue {
     _platform.onWrite = onWrite;
   }
 
-  static Future<void> discoverServices(String deviceId) => _platform.discoverServices(deviceId);
+  static Future<void> discoverServices(String deviceId) =>
+      _platform.discoverServices(deviceId);
 
   static void setServiceHandler(OnServiceDiscovered? onServiceDiscovered) {
     _platform.onServiceDiscovered = onServiceDiscovered;
   }
 
-  static Future<void> setNotifiable(String deviceId, String service, String characteristic, BleInputProperty bleInputProperty) {
-    return _platform.setNotifiable(deviceId, service, characteristic, bleInputProperty);
+  static Future<void> setNotifiable(String deviceId, String service,
+      String characteristic, BleInputProperty bleInputProperty) {
+    return _platform.setNotifiable(
+        deviceId, service, characteristic, bleInputProperty);
   }
 
   static void setValueHandler(OnValueChanged? onValueChanged) {
     _platform.onValueChanged = onValueChanged;
   }
 
-  static Future<void> readValue(String deviceId, String service, String characteristic) {
+  static Future<void> readValue(
+      String deviceId, String service, String characteristic) {
     return _platform.readValue(deviceId, service, characteristic);
   }
 
-  static Future<void> writeValue(String deviceId, String service, String characteristic, Uint8List value, BleOutputProperty bleOutputProperty) {
-    return _platform.writeValue(deviceId, service, characteristic, value, bleOutputProperty);
+  static Future<void> writeValue(
+      String deviceId,
+      String service,
+      String characteristic,
+      Uint8List value,
+      BleOutputProperty bleOutputProperty) {
+    return _platform.writeValue(
+        deviceId, service, characteristic, value, bleOutputProperty);
   }
 
-  static Future<int> requestMtu(String deviceId, int expectedMtu) => _platform.requestMtu(deviceId, expectedMtu);
+  static Future<int> requestMtu(String deviceId, int expectedMtu) =>
+      _platform.requestMtu(deviceId, expectedMtu);
 }
