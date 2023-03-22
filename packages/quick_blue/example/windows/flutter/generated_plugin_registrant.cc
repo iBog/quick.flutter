139eb71a7c414ae18367efc14a7c440b4bb209f5
iBog <iBog@users.noreply.github.com>
2023-03-22 18:58:54 +0300

    no logging

--- packages/quick_blue/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/quick_blue/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <quick_blue/quick_blue_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  QuickBluePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("QuickBluePlugin"));
 }
--- packages/quick_blue/example/windows/flutter/generated_plugins.cmake
+++ packages/quick_blue/example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  quick_blue
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
