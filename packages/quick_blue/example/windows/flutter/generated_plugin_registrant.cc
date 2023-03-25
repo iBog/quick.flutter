2eb12fae22e23dccda5381043bfa97d560b3eafe
iBog <iBog@users.noreply.github.com>
2023-03-25 12:49:27 +0300

    return Windows/Linux

--- packages/quick_blue/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/quick_blue/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <quick_blue/quick_blue_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  QuickBluePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("QuickBluePlugin"));
 }
--- packages/quick_blue/example/windows/flutter/generated_plugins.cmake
+++ packages/quick_blue/example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  quick_blue
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
