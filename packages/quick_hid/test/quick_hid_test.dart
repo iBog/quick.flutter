edecb7ce3cb8d7d4904b260c45a968d6df6407ca
iBog <iBog@users.noreply.github.com>
2023-11-28 15:18:26 +0300

    updated deps

--- packages/quick_hid/test/quick_hid_test.dart
+++ packages/quick_hid/test/quick_hid_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:quick_hid/quick_hid.dart';
+
+import '../lib/quick_hid.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('quick_hid');
