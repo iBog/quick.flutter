cb15ddb546ab87c3053138afc03039d3b4ac8a36
iBog <iBog@users.noreply.github.com>
2024-03-14 18:28:25 +0300

    fix build

--- packages/quick_blue/android/src/main/kotlin/com/example/quick_blue/QuickBluePlugin.kt
+++ packages/quick_blue/android/src/main/kotlin/com/example/quick_blue/QuickBluePlugin.kt
@@ -17,6 +17,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.*
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.util.*
